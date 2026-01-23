833d1e6622945ff5a604a33ffff657c166c83220
m4jorskyy <igor.suchodolski@student.put.poznan.pl>
2026-01-23 17:43:01 +0100

    gapi

--- src/api/google/gapi.ts
+++ src/api/google/gapi.ts
@@ -1,6 +1,6 @@
 import api from "../api";
 
 export const sendGoogleCode = async (code: string, userId: number) => {
-    const { data } = await api.post("/google/calendar/auth/", { code, userId });
+    const { data } = await api.post("/google/calendar/auth", { code, userId });
     return data;
 };
